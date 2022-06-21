b043cf217c36af340750f4ab7074ade338cfbca4
DLZIN <103215171+DLZIN@users.noreply.github.com>
2022-06-21 01:02:19 -0300

    Add files via upload

---
+++ ban.py
@@ -0,0 +1,36 @@
+#By DLZ!N
+#2k22 golpeado bobão
+import sys
+import time
+import os 
+
+#colors 
+RED = "\033[1;31m"
+BLUE = "\033[1;34m"
+CYAN = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD = "\033[;1m"
+REVERSE = "\033[;7m"
+CINZA = "	\033[1;90m"
+
+print(CYAN,
+	'============================================ ')
+print('================',RED,'DL BAN 1.2',CYAN,'================')
+print('============================================',CINZA)
+print('''Esse metodo e privado não compartilhe.
+By DLZ!N''')
+print(RED,'digite seu usuário:',BLUE)
+n=input('')
+print(RED,'digite sua senha:',BLUE)
+s=input('')
+
+os.system ('clear')
+
+print(RED,'numero da vítima:',BLUE)
+k=input('')
+print(RED,'enviando spam denúncias..'+GREEN)
+for x in range(999999):
+  print("denúncias enviadas: {}".format (x))
+  sys.stdout.write("\033[F")
+  time.sleep(0.2)
